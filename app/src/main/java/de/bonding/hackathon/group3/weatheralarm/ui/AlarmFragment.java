7596dd634cf4fdfe7b10df22ee6187d3a7580f6a
Mike Klimek <m.klimek@tu-bs.de>
2016-11-18 12:56:36 +0100

    Add ViewPager + Tabs to switch between the two main fragments

---
+++ app/src/main/java/de/bonding/hackathon/group3/weatheralarm/ui/AlarmFragment.java
@@ -0,0 +1,19 @@
+package de.bonding.hackathon.group3.weatheralarm.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.bonding.hackathon.group3.weatheralarm.R;
+
+public class AlarmFragment extends Fragment {
+    private static final String TAG = AlarmFragment.class.getSimpleName();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_alarm, container, false);
+        return rootView;
+    }
+}
--- app/src/main/java/de/bonding/hackathon/group3/weatheralarm/ui/MainActivity.java
+++ app/src/main/java/de/bonding/hackathon/group3/weatheralarm/ui/MainActivity.java
@@ -1,31 +1,45 @@
-package de.bonding.hackathon.group3.weatheralarm;
+package de.bonding.hackathon.group3.weatheralarm.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import de.bonding.hackathon.group3.weatheralarm.R;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CoordinatorLayout mCoordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
+
+        initToolbar();
+        initTabs();
+    }
+
+
+    private void initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    private void initTabs() {
+        AlarmPagerAdapter adapter = new AlarmPagerAdapter(getFragmentManager(), getResources());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
