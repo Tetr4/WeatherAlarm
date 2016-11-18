02518f4fcc4fb6aac5f6892bc363bff8f23b0900
Mike Klimek <m.klimek@tu-bs.de>
2016-11-18 15:53:17 +0100

    Fix query in OpenWeatherMapService

--- app/src/main/java/de/bonding/hackathon/group3/weatheralarm/openweathermap/OpenWeatherMapService.java
+++ app/src/main/java/de/bonding/hackathon/group3/weatheralarm/openweathermap/OpenWeatherMapService.java
@@ -3,10 +3,10 @@ package de.bonding.hackathon.group3.weatheralarm.openweathermap;
 import de.bonding.hackathon.group3.weatheralarm.model.WeatherInfo;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface OpenWeatherMapService {
 
-    @GET("data/2.5/weather/?lat={lat}&lon={lon}")
-    Call<WeatherInfo> getWeatherInfo(@Path("lat") String lat, @Path("lon") String lon);
+    @GET("data/2.5/weather/")
+    Call<WeatherInfo> getWeatherInfo(@Query("lat") String lat, @Query("lon") String lon);
 }
