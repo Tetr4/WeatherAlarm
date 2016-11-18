c3c42b9b99dcea5c0e653489dd960d9959294019
Mike Klimek <m.klimek@tu-bs.de>
2016-11-18 19:06:52 +0100

    Remove unused code

--- app/src/main/java/de/bonding/hackathon/group3/weatheralarm/WeatherAlarmApplication.java
+++ app/src/main/java/de/bonding/hackathon/group3/weatheralarm/WeatherAlarmApplication.java
@@ -5,8 +5,6 @@ import android.app.Application;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import de.bonding.hackathon.group3.weatheralarm.model.WeatherInfo;
-import de.bonding.hackathon.group3.weatheralarm.openweathermap.OpenWeatherMapDeserializer;
 import de.bonding.hackathon.group3.weatheralarm.openweathermap.OpenWeatherMapService;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
@@ -23,17 +21,5 @@ public class WeatherAlarmApplication extends Application {
     }
 
     private void initService() {
-        OkHttpClient client = new OkHttpClient();
-
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(WeatherInfo.class, new OpenWeatherMapDeserializer(client))
-                .create();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://api.openweathermap.org/")
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .client(client)
-                .build();
-        sWeatherService = retrofit.create(OpenWeatherMapService.class);
     }
 }
